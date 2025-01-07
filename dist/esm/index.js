483db8031b1b5978ce82717541dc0b1607473aff
Colin Capurso <colin.capurso@enabled.com.au>
2025-01-07 19:33:27 +1030

    Added cjs, esm. Moved to src directory.

---
+++ dist/esm/index.js
@@ -0,0 +1,13 @@
+export var BusinessRole;
+(function (BusinessRole) {
+    BusinessRole["RideOperator"] = "ride-operator";
+    BusinessRole["EventHost"] = "event-host";
+    BusinessRole["InternalSupport"] = "internal-support";
+})(BusinessRole || (BusinessRole = {}));
+export var RegistrationStatus;
+(function (RegistrationStatus) {
+    RegistrationStatus["idle"] = "idle";
+    RegistrationStatus["accepted"] = "accepted";
+    RegistrationStatus["rejected"] = "rejected";
+    RegistrationStatus["standby"] = "standby";
+})(RegistrationStatus || (RegistrationStatus = {}));
