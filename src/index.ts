c782c5bb0eba81654dd366c2608d4d90200105de
Colin Capurso <colin.capurso@enabled.com.au>
2025-01-13 20:06:09 +1030

    Updated ride document to include hidden values

--- src/index.ts
+++ src/index.ts
@@ -164,6 +164,16 @@ export type RideCommon = {
   _products_range: string,
   _total_site_dimensions: string,
   _device_dimensions: string
+
+  // Hidden from user
+  _plant_rego_state_issued: string
+  _plant_rego_number: string
+  _plant_rego_expiry_date: string
+  _engineering_cert_inspected_date: string
+  _engineering_cert_engineer_name: string
+  _public_liability_policy_number: string
+  _public_liability_expiry_date: string
+  _public_liability_coverage_amount: string
 }
 
 export interface RideDocument extends RideCommon {
