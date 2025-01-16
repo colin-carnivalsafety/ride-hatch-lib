fe24b1115b000c2fd37dd8b80ba24cd3e718666a
Colin Capurso <colin.capurso@enabled.com.au>
2025-01-16 13:54:41 +1030

    Updated build script.

--- dist/cjs/index.js
+++ dist/cjs/index.js
@@ -1,12 +1,17 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.RegistrationStatus = exports.BusinessRole = void 0;
+exports.RegistrationStatus = exports.BusinessSubscription = exports.BusinessRole = void 0;
 var BusinessRole;
 (function (BusinessRole) {
     BusinessRole["RideOperator"] = "ride-operator";
     BusinessRole["EventHost"] = "event-host";
     BusinessRole["InternalSupport"] = "internal-support";
 })(BusinessRole || (exports.BusinessRole = BusinessRole = {}));
+var BusinessSubscription;
+(function (BusinessSubscription) {
+    BusinessSubscription["SubscriptionRideOperatorRahs"] = "subscription-ro-rahs";
+    BusinessSubscription["SubscriptionRideOperatorIntroductory"] = "subscription-ro-introductory";
+})(BusinessSubscription || (exports.BusinessSubscription = BusinessSubscription = {}));
 var RegistrationStatus;
 (function (RegistrationStatus) {
     RegistrationStatus["idle"] = "idle";
